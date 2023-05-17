1114baff1e6ff7e715bc3ca68d0c2728ee0c6e15
Mosena <Mosena@10.33.43.11>
2023-05-17 13:53:34 +0700

    init

---
+++ src/main/java/swp/quizpracticingsystem/QuizpracticingsystemApplication.java
@@ -0,0 +1,11 @@
+package swp.quizpracticingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizpracticingsystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizpracticingsystemApplication.class, args);
+    }
+}
