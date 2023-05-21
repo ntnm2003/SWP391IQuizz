df741cf48a795d744eac8cdda6d5afa06d56961f
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2023-05-21 18:22:43 +0700

    editing homepage

---
+++ src/main/java/swp/quizpracticingsystem/controller/HomeController.java
@@ -0,0 +1,13 @@
+package swp.quizpracticingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+	@GetMapping("/home")
+	public String getToHomePage() {
+		return "home/homepage";
+	}
+}
