b6f5118808e0062c78e821c9c35ab6946e00a85c
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2023-05-22 22:01:10 +0700

    updated header.html and footer.html

--- src/main/java/swp/quizpracticingsystem/controller/BlogController.java
+++ src/main/java/swp/quizpracticingsystem/controller/BlogController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import swp.quizpracticingsystem.model.Posts;
 import swp.quizpracticingsystem.service.IBlogService;
 
 @Controller
+@RequestMapping("/blogs")
 public class BlogController {
 	
 	@Autowired
