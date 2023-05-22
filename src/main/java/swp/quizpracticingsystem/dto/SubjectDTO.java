2e95ab2afd35d47ffd2829f06f3ded7deb9a91ea
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2023-05-23 03:32:24 +0700

    AQ 22/5/2023

--- src/main/java/swp/quizpracticingsystem/dto/SubjectDTO.java
+++ src/main/java/swp/quizpracticingsystem/dto/SubjectDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import swp.quizpracticingsystem.model.Subject;
 
 @Getter
 @Setter
@@ -17,4 +18,46 @@ public class SubjectDTO {
     private List<UserDTO> expertId;
     private String courseName;
     private Date dateStart;
+
+    public Integer getIdCourse() {
+        return idCourse;
+    }
+
+    public void setIdCourse(Integer idCourse) {
+        this.idCourse = idCourse;
+    }
+
+    public CategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryDTO category) {
+        this.category = category;
+    }
+
+    public List<UserDTO> getExpertId() {
+        return expertId;
+    }
+
+    public void setExpertId(List<UserDTO> expertId) {
+        this.expertId = expertId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+
 }
--- src/main/java/swp/quizpracticingsystem/model/Posts.java
+++ src/main/java/swp/quizpracticingsystem/model/Posts.java
@@ -103,4 +103,6 @@ public class Posts {
 	public void setBriefInfor(String briefInfor) {
 		this.briefInfor = briefInfor;
 	}
+
+
 }
