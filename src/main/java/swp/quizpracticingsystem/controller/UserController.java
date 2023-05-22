456535b1280c5bc4331990e018d8f24ccdee1d08
Nguyen Van Vui <vuinvhe176325@fpt.edu.vn>
2023-05-22 12:32:32 +0700

    login

--- src/main/java/swp/quizpracticingsystem/controller/UserController.java
+++ src/main/java/swp/quizpracticingsystem/controller/UserController.java
@@ -1,17 +1,19 @@
 package swp.quizpracticingsystem.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import swp.quizpracticingsystem.model.User;
+import swp.quizpracticingsystem.service.UserService;
 
 @Controller
 public class UserController {
 
     @Autowired
-    
+    UserService userService;
     @GetMapping("/register")
     public String register() {
         return "home/register";
@@ -20,6 +22,17 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "home/login";
+    }
+    @PostMapping("/login")
+    public String login(HttpServletRequest request, HttpSession session) {
+        User account = userService.login(request.getParameter("email"), request.getParameter("password"));
+        if(account == null) {
+            session.setAttribute("message", "Invalid username or password.");
+            return "home/login";
+        }else {
+            session.setAttribute("user", account);
+            return "home/homepage/homepage";
+        }
     }
 
 }
