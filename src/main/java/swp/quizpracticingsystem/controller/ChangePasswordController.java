dfe3e644fe03954fd4f77307de97fa6b25fa195a
starrune2003 <starrune123@gmail.com>
2023-05-23 18:58:24 +0700

    reset_password

--- src/main/java/swp/quizpracticingsystem/controller/ChangePasswordController.java
+++ src/main/java/swp/quizpracticingsystem/controller/ChangePasswordController.java
@@ -5,11 +5,15 @@
 package swp.quizpracticingsystem.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import swp.quizpracticingsystem.model.User;
 import swp.quizpracticingsystem.repository.IAccountRepository;
 import swp.quizpracticingsystem.service.IAccountService;
 import swp.quizpracticingsystem.serviceImple.AccountService;
@@ -37,10 +41,31 @@ public class ChangePasswordController {
     
     @PostMapping("/change_password")
     public String processChangePassword(HttpServletRequest request, Model model){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         
         String oldPassword = request.getParameter("oldPass");
         String newPassword = request.getParameter("newPass");
         String confirmPassword = request.getParameter("confirmPass");
-        return "login";
+        HttpSession session = request.getSession(false);
+        
+        if (session != null && session.getAttribute("user") != null) {
+            User user = (User) session.getAttribute("user");
+            if (passwordEncoder.matches(oldPassword, user.getPassword())) {
+                if(newPassword.matches(confirmPassword)){
+                user.setPassword(passwordEncoder.encode(newPassword));
+                accountService.updatePassword(user, newPassword);
+                return "redirect:/home";
+                }else{
+                    model.addAttribute("error2","Wrong re-enter new password");
+                    return "password/change_password_form";
+                }
+            } else {
+                model.addAttribute("error1", "Wrong old password");
+                return "password/change_password_form";
+            }
+        } else {
+            return "redirect:/login";
+        }
+            
     }
 }
