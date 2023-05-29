5a6034b1248151448ca1fa68f34559202b2a3a5f
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2023-05-29 15:59:21 +0700

    update url for blog-detail and subject-detail

--- src/main/java/swp/quizpracticingsystem/controller/SubjectDetailController.java
+++ src/main/java/swp/quizpracticingsystem/controller/SubjectDetailController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import swp.quizpracticingsystem.model.SubjectOverview;
 import swp.quizpracticingsystem.service.ISubjectOverviewService;
 
@@ -13,8 +14,8 @@ public class SubjectDetailController {
     @Autowired
     private ISubjectOverviewService subjectOverviewService;
 
-    @GetMapping("/subject/subject-detail/{id}")
-    public String showSubjectDetails(@PathVariable("id") Integer id, Model model) {
+    @GetMapping("/subjects/subject-detail")
+    public String showSubjectDetails(@RequestParam(name = "id", required = true) Integer id, Model model) {
         SubjectOverview subject = subjectOverviewService.getSubjectById(id);
         model.addAttribute("subject", subject);
         return "sub_details/sub_details";
