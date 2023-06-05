f759b0aada05374fed873c4d39ee3fe23d060449
Mosena <Mosena@192.168.108.102>
2023-06-06 01:07:56 +0700

    Revert "update model and folders(1)"
    This reverts commit cd5cbecee6f6d8307e1376dc42cf058a29ca1534.

--- src/main/java/swp391/quizpracticing/model/Blogcategory.java
+++ src/main/java/swp391/quizpracticing/model/Blogcategory.java
@@ -4,8 +4,6 @@ package swp391.quizpracticing.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -27,7 +25,6 @@ import lombok.Setter;
 public class Blogcategory {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "name")
