edf456cfcab57719d191d0c728b1546ee53df548
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-05 09:36:32 +0000

    Delete IPricePackageRepo.java

--- src/main/java/swp/quizpracticingsystem/repository/IPricePackageRepo.java
+++
@@ -1,8 +0,0 @@
-package swp.quizpracticingsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import swp.quizpracticingsystem.model.PricePackage;
-
-public interface IPricePackageRepo extends JpaRepository<PricePackage, Integer> {
-
-}
