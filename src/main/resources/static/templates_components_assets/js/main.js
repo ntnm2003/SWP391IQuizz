5ecfbd7c087ebe30879bd55199532a5cf9c04ef1
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-05 09:43:17 +0000

    Delete main.js

--- src/main/resources/static/templates_components_assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-let navigation = document.querySelector('.navbar__content');
-let menuToggle = document.querySelector('.menuToggle');
-let header = document.querySelector('header');
-
-menuToggle.onclick = function () {
-    header.classList.toggle('open');
-}
-
