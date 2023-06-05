b9d4a297ef46446a3054fadfd191d843e0432a03
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-05 09:28:53 +0000

    Delete UserNotFoundException.java

--- src/main/java/swp/quizpracticingsystem/NotFound/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package swp.quizpracticingsystem.NotFound;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
