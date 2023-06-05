d2da83f64744568b71379ec8f2a9160985d10c5a
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-05 09:36:50 +0000

    Delete ISubjectDetailRepository.java

--- src/main/java/swp/quizpracticingsystem/repository/ISubjectDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package swp.quizpracticingsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import swp.quizpracticingsystem.model.SubjectDetail;
-
-import java.util.List;
-
-public interface ISubjectDetailRepository extends JpaRepository<SubjectDetail, Integer> {
-}
