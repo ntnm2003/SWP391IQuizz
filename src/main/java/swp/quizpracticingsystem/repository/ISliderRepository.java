195f7d8d7cdbdc2ac741f47e36f34a16e1f88e20
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-05 09:36:44 +0000

    Delete ISliderRepository.java

--- src/main/java/swp/quizpracticingsystem/repository/ISliderRepository.java
+++
@@ -1,9 +0,0 @@
-package swp.quizpracticingsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import swp.quizpracticingsystem.model.Slider;
-
-public interface ISliderRepository extends JpaRepository<Slider, Integer> {
-
-}
