e513bd8aea02f6e7b217c09a3fbae1149ee978fa
Mosena <Mosena@192.168.108.102>
2023-06-06 19:33:35 +0700

    quick fix

--- src/main/java/swp391/quizpracticing/repository/IUserRepository.java
+++ src/main/java/swp391/quizpracticing/repository/IUserRepository.java
@@ -4,7 +4,6 @@
  */
 package swp391.quizpracticing.repository;
 
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -14,6 +13,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import swp391.quizpracticing.model.Role;
 import swp391.quizpracticing.model.User;
 
 /**
@@ -34,12 +34,12 @@ public interface IUserRepository  extends JpaRepository<User,Integer>
     public User save(User u);
     
     @Modifying
-    @Query("UPDATE User u SET u.status = :status WHERE u.id = :userId")
+    @Query("UPDATE User u SET u.enable = :status WHERE u.id = :userId")
     void updateUserStatus(@Param("userId") Integer userId, 
             @Param("status") Boolean status);
     
     @Modifying
-    @Query("UPDATE User u SET u.role_id = :role WHERE u.id = :userId")
+    @Query("UPDATE User u SET u.role = :role WHERE id = :userId")
     void updateUserRole(@Param("userId") Integer userId, 
-            @Param("roleId") Integer roleId);
+            @Param("role") Role role);
 }
