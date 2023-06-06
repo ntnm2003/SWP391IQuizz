12de3b891593e85cbf84c6376e2b8009150eaf78
Nguyen Van Vui <vuinvhe176325@fpt.edu.vn>
2023-06-06 21:24:04 +0700

    abc

--- src/main/java/swp391/quizpracticing/model/User.java
+++ src/main/java/swp391/quizpracticing/model/User.java
@@ -1,10 +1,11 @@
 package swp391.quizpracticing.model;
 
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -25,8 +26,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+
     private Integer id;
 
     @Column(name = "avatar")
@@ -55,23 +59,23 @@ public class User {
 
     @Column(name = "enable")
     private Boolean enable;
-    
-    @OneToMany(mappedBy = "author",cascade = CascadeType.ALL)
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     private List<Blog> blogs;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id",referencedColumnName = "id")
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
     private Role role;
-    
+
     @OneToMany(mappedBy = "owner")
     private List<Subject> subject;
-    
+
     @OneToMany(mappedBy = "creator")
     private List<Lesson> lessons;
-    
+
     @OneToMany(mappedBy = "user")
     private List<Quizreview> quizReviews;
-    
+
     @OneToMany(mappedBy = "id.user")
     private List<UserSubject> userSubjects;
 }
