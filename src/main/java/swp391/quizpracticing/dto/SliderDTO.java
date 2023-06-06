8302c5dd3879e86ac37871f737be3e2b07cbe416
laitrangg <tranglkhe176485@fpt.edu.vn>
2023-06-06 22:59:52 +0700

    update sliders list

--- src/main/java/swp391/quizpracticing/dto/SliderDTO.java
+++ src/main/java/swp391/quizpracticing/dto/SliderDTO.java
@@ -15,4 +15,15 @@ public class SliderDTO {
     private String image;
     private String backLink;
     private Boolean status;
+
+    public void sliderHomePage(Slider slider) {
+        this.postId = post.getPostId();
+        this.thumbnail = post.getThumbnail();
+        this.title = post.getTitle();
+        this.updatedDate = post.getUpdatedDate();
+        this.briefInfor = post.getBriefInfor();
+        PostCategoryDTO postCategoryDTO = new PostCategoryDTO();
+        postCategoryDTO.entityToDTO(post.getPostCategory());
+        this.postCategory = postCategoryDTO;
+    }
 }
