15b50dcc4088ae56f393d0a9f6cfeced7737384c
Mosena <Mosena@192.168.108.102>
2023-06-07 16:58:06 +0700

    update admin features(2)

--- src/main/java/swp391/quizpracticing/Utility/Utility.java
+++ src/main/java/swp391/quizpracticing/Utility/Utility.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package swp.quizpracticingsystem.Utility;
+package swp391.quizpracticing.Utility;
 
 import jakarta.servlet.http.HttpServletRequest;
 
