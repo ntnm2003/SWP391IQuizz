e1ca188ab12a63e2c94ed1270ce3f747f1bbffeb
Mosena <Mosena@192.168.108.102>
2023-06-08 14:31:24 +0700

    Revert "quick fix bug"
    This reverts commit 201f5b5f0cefd3bb73899d4c3a253ca939df01fe.

--- src/main/java/swp391/quizpracticing/serviceimple/LoginServiceImpl.java
+++ src/main/java/swp391/quizpracticing/serviceimple/LoginServiceImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package swp391.quizpracticing.serviceimple;
+package swp391.quizpracticing.serviceImple;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
