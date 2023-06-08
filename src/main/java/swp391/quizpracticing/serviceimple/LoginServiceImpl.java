2f17164a875cbcdc36022ebc4c4e6c626d2d0d36
Mosena <Mosena@192.168.108.102>
2023-06-08 12:32:45 +0700

    quick fix bug

--- src/main/java/swp391/quizpracticing/serviceimple/LoginServiceImpl.java
+++ src/main/java/swp391/quizpracticing/serviceimple/LoginServiceImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package swp391.quizpracticing.serviceImple;
+package swp391.quizpracticing.serviceimple;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
