8621ff2bce1afa0cfccb785e5bbc621d59fbfb0a
Mosena <Mosena@192.168.108.102>
2023-06-08 09:56:50 +0700

    finish admin features

--- src/main/resources/static/js/admin/admin.js
+++ src/main/resources/static/js/admin/admin.js
@@ -5,23 +5,26 @@ function setPageFilters() {
     const role = urlParams.get("role");
     const sortBy = urlParams.get("sortBy");
     const order = urlParams.get("order");
-  
+    const searchValue=urlParams.get("searchValue");
     if (gender) {
-      document.getElementById("genderSelect").value = gender;
+        document.getElementById("genderSelect").value = gender;
     }
     if (role) {
-      document.getElementById("roleSelect").value = role;
+        document.getElementById("roleSelect").value = role;
     }
     if (status_) {
-      document.getElementById("statusSelect").value = status_;
+        document.getElementById("statusSelect").value = status_;
     }
     if (sortBy) {
-      document.getElementById("sortBySelect").value = sortBy;
+        document.getElementById("sortBySelect").value = sortBy;
     }
     if (order) {
-      document.getElementById("orderSelect").value = order;
+        document.getElementById("orderSelect").value = order;
     }
-  }
+    if(searchValue){
+        document.getElementById("search").value=searchValue;
+    }
+}
   
 window.addEventListener("load", setPageFilters);
 function navigateToInteract() {
@@ -30,10 +33,14 @@ function navigateToInteract() {
     var role = document.getElementById("roleSelect").value;
     var sortBy=document.getElementById("sortBySelect").value;
     var order=document.getElementById("orderSelect").value;
+    var searchValue=document.getElementById("search").value;
 
     var url = "/admin/user-list?";
     var filters = [];
 
+    if(searchValue){
+        filters.push("searchValue=" + searchValue);
+    }
     if (gender) {
         filters.push("gender=" + gender);
     }
@@ -51,21 +58,24 @@ function navigateToInteract() {
     }
     url += filters.join("&");
     
-
-    // Navigate to the filtered URL
     window.location.href = url;
+    
 }
-
 function moveOn(pageNo){
     var gender = document.getElementById("genderSelect").value;
     var status = document.getElementById("statusSelect").value;
     var role = document.getElementById("roleSelect").value;
     var sortBy=document.getElementById("sortBySelect").value;
     var order=document.getElementById("orderSelect").value;
+    var searchValue=document.getElementById("search").value;
+
+    var url = "/admin/user-list?pageNo=" + (parseInt(pageNo) + 1)+"&";
     
-    var url = "/admin/user-list?pageNo=" + (parseInt(pageNo) + 1) + "&";
     var filters = [];
 
+    if(searchValue){
+        filters.push("searchValue=" + searchValue);
+    }
     if (gender) {
         filters.push("gender=" + gender);
     }
@@ -92,10 +102,15 @@ function moveBack(pageNo){
     var role = document.getElementById("roleSelect").value;
     var sortBy=document.getElementById("sortBySelect").value;
     var order=document.getElementById("orderSelect").value;
+    var searchValue=document.getElementById("search").value;
 
-    var url = "/admin/user-list?pageNo=" + (parseInt(pageNo) - 1) + "&";
+    var url = "/admin/user-list?pageNo=" + (parseInt(pageNo) - 1)+"&";
+    
     var filters = [];
 
+    if(searchValue){
+        filters.push("searchValue=" + searchValue);
+    }
     if (gender) {
         filters.push("gender=" + gender);
     }
@@ -114,4 +129,11 @@ function moveBack(pageNo){
     url += filters.join("&");
     
     window.location.href = url;
+}
+function showPopup() {
+    document.getElementById("popupForm").style.display = "block";
+}
+  
+function hidePopup() {
+    document.getElementById("popupForm").style.display = "none";
 }
