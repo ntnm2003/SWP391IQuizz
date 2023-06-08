3336b5440a27b3c602bafb7477987c7c955397c5
Mosena <Mosena@192.168.108.102>
2023-06-08 16:24:57 +0700

    quick fix bug

--- src/main/java/swp391/quizpracticing/controller/AdminController.java
+++ src/main/java/swp391/quizpracticing/controller/AdminController.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -17,14 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import swp391.quizpracticing.Utility.Utility;
+import swp391.quizpracticing.Utils.Utility;
 import swp391.quizpracticing.dto.RoleDTO;
 import swp391.quizpracticing.dto.SettingsDTO;
 import swp391.quizpracticing.dto.UserDTO;
@@ -40,20 +40,19 @@ import swp391.quizpracticing.serviceimple.UserService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+    
     @Autowired
     private IUserService userService;
-
+    
     @Autowired
     private ISettingsService settingsService;
-
+    
     @Autowired
     private IRoleService roleService;
-
+    
     @Autowired
     private JavaMailSenderImpl mailSender;
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    
     @GetMapping("/user-list")
     public String searchUser(HttpSession session,
             Model model,
@@ -61,20 +60,20 @@ public class AdminController {
             @RequestParam(name="searchValue", required = false) String searchValue,
             @RequestParam(name = "gender", 
                     required = false) Boolean gender,
-            @RequestParam(name = "status", 
+            @RequestParam(name = "enable", 
                     required = false) Boolean status,
             @RequestParam(name ="role",
                     required=false) Integer roleId,
             @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
             @RequestParam(name = "order",defaultValue = "asc") String order){
         int pageSize=6;
-        Page<UserDTO> users=userService.getUsers(pageNo, pageSize,
+        Page<UserDTO> users=userService.getUsers(pageNo, pageSize, 
                 searchValue, gender, status, roleId, sortBy, order);
         List<RoleDTO> roles=roleService.findRoles();
         int totalPages=users.getTotalPages();
         if(totalPages==0 || pageNo<1 || pageNo>totalPages){
             model.addAttribute("msg", "Not found");
-            return "admin/error";
+            return "/admin/error";
         }
         UserDTO user = userService.findUser(10);
         session.setAttribute("user", user);
@@ -83,16 +82,16 @@ public class AdminController {
         model.addAttribute("totalPages",totalPages);
         model.addAttribute("users", users);
         model.addAttribute("userSession", session.getAttribute("user"));
-        return "admin/userlist";
+        return "/admin/userlist";
     }
-
+    
     @GetMapping("/settings")
     public String searchSetting(HttpSession session,
             Model model,
             @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
             @RequestParam(name="value", required = false) String value,
             @RequestParam(name="type", required = false) String type,
-            @RequestParam(name="status",required = false) Boolean status,
+            @RequestParam(name="enable",required = false) Boolean status,
             @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
             @RequestParam(name = "order",defaultValue = "asc") String order){
         int pageSize=6;
@@ -103,7 +102,7 @@ public class AdminController {
         int totalPages=settings.getTotalPages();
         if(totalPages==0 || pageNo<1 || pageNo>totalPages){
             model.addAttribute("msg", "Not found");
-            return "admin/error";
+            return "/admin/error";
         }
         UserDTO user = userService.findUser(10);
         session.setAttribute("user", user);
@@ -112,9 +111,9 @@ public class AdminController {
         model.addAttribute("totalPages",totalPages);
         model.addAttribute("settings", settings);
         model.addAttribute("userSession", session.getAttribute("user"));
-        return "admin/settings";
+        return "/admin/settings";
     }
-
+    
     @GetMapping("/user-detail")
     public String getUser(HttpSession session, @RequestParam("userId") Integer userId, Model model){
         UserDTO u=userService.findUser(userId);
@@ -126,7 +125,6 @@ public class AdminController {
         model.addAttribute("user", u);
         return "/admin/userdetail";
     }
-
     @GetMapping("/setting-detail")
     public String getSetting(HttpSession session,@RequestParam("settingId") Integer settingId, Model model){
         SettingsDTO s=settingsService.findById(settingId);
@@ -138,11 +136,10 @@ public class AdminController {
         model.addAttribute("setting",s);
         return "/admin/settingdetail";
     }
-
     @PostMapping("/user-detail/change")
     public String updateUser(HttpSession session,@RequestParam("id") Integer id, 
             @RequestParam("role") Integer roleId,
-            @RequestParam("status") Boolean status,
+            @RequestParam("enable") Boolean status,
             Model model){
         UserDTO user = userService.findUser(10);
         session.setAttribute("user", user);
@@ -150,14 +147,14 @@ public class AdminController {
         userService.updateUser(id, roleId, status);
         return "redirect:/admin/user-detail?userId="+id;
     }
-
+    
     @PostMapping("/add-user")
     public String addUser(HttpSession session,@RequestParam("fullName") String fullName,
             @RequestParam("role") Integer roleId,
             @RequestParam("email") String email,
             HttpServletRequest request, Model model){
         if(userService.findUserByEmail(email)){
-            model.addAttribute("msg",
+            model.addAttribute("msg", 
                     "Email has already existed");
             return "/admin/error";
         }
@@ -175,11 +172,10 @@ public class AdminController {
         u.setPassword(randomPass);
         u.setEnable(false);
         userService.addUser(u);
-        sendVerification(fullName, email,
+        sendVerification(fullName, email, 
                 randomCode,randomPass, request);
         return "redirect:/admin/user-list";
     }
-
     @PostMapping("/setting-detail/change")
     public String updateSetting(HttpSession session, Model model,@RequestParam("id") Integer id, 
             @RequestParam("value") String value,
@@ -192,7 +188,7 @@ public class AdminController {
         settingsService.updateSettings(id, value, order, status, description);
         return "redirect:/admin/setting-detail?settingId="+id;
     }
-
+    
     @PostMapping("/add-setting")
     public String addSetting(
             HttpSession session,
@@ -207,7 +203,6 @@ public class AdminController {
         settingsService.addSetting(type, order, value, description);
         return "redirect:/admin/settings";
     }
-
     @GetMapping("/verify")
     public String verify(@RequestParam("code") String code, Model model){
         UserDTO u=userService.findUserByToken(code);
@@ -218,7 +213,6 @@ public class AdminController {
         model.addAttribute("msg", "Get lost?");
         return "/admin/error";
     }
-
     @GetMapping("/discard")
     public String discard(@RequestParam("code") String code, Model model){
         UserDTO u=userService.findUserByToken(code);
@@ -229,10 +223,10 @@ public class AdminController {
         model.addAttribute("msg", "Get lost?");
         return "/admin/error";
     }
-
+    
     public void sendVerification(String name, String email, String token
             ,String password,HttpServletRequest request){
-        String siteURL= Utility.getSiteURL(request);
+        String siteURL=Utility.getSiteURL(request);
         String toAddress = email;
         String fromAddress = "gmail@sss";
         String senderName = "IQuizz";
@@ -267,3 +261,4 @@ public class AdminController {
         mailSender.send(message);
     }
 }
+
