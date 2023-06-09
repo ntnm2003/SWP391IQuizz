c6c929149bc0d8ef0d139307b9ecb6e3d06fe4fb
Mosena <Mosena@192.168.108.102>
2023-06-09 23:23:00 +0700

    fix authority and authorization last(2)

--- src/main/java/swp391/quizpracticing/config/SimpleAuthenticationSuccessHandler.java
+++ src/main/java/swp391/quizpracticing/config/SimpleAuthenticationSuccessHandler.java
@@ -11,20 +11,31 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import swp391.quizpracticing.model.User;
 
 @Configuration
 public class SimpleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     
+    
+    private RequestCache requestCache = new HttpSessionRequestCache();
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     private HttpSession session;
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, 
+            HttpServletResponse response, Authentication authentication) 
+            throws IOException, ServletException {
         User user=(User)authentication.getPrincipal();
         session=request.getSession();
         session.setAttribute("user", user);
-        redirectStrategy.sendRedirect(request, response, request.getContextPath());
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        redirectStrategy.sendRedirect(request, response, savedRequest.getRedirectUrl());
+    }
+    public void setRequestCache(RequestCache requestCache) {
+        this.requestCache = requestCache;
     }
 }
