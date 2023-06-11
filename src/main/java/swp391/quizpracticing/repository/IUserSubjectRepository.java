65cfe1d47e4893e8c752428081f7fce1b129515b
Mosena <Mosena@192.168.108.102>
2023-06-12 00:20:19 +0700

    fix authority and authorization last(2)

--- src/main/java/swp391/quizpracticing/repository/IUserSubjectRepository.java
+++ src/main/java/swp391/quizpracticing/repository/IUserSubjectRepository.java
@@ -12,14 +12,28 @@ import swp391.quizpracticing.model.UserSubject;
 import swp391.quizpracticing.model.UserSubjectKey;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  *
  * @author Mosena
  */
 @Repository
-public interface IUserSubjectRepository extends JpaRepository<UserSubject,UserSubjectKey> {
+public interface IUserSubjectRepository 
+        extends JpaRepository<UserSubject,UserSubjectKey>,
+        JpaSpecificationExecutor<UserSubject> {
 
     @Query(value = "select * from iquiz.user_subject where user_id = :user_id and registration_status_id = :registration_status_id", nativeQuery = true)
     public List<UserSubject> getAllByUserIdAndRegistrationStatus(@Param("user_id") Integer userId, @Param("registration_status_id") Integer registrationId);
+
+    @Override
+    public Page<UserSubject> findAll(Specification specifiation,Pageable page);
+    
+    @Override
+    public UserSubject save(UserSubject us);
+    
+    public void update
 }
