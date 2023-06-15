a16d831476f7d2eb35edd32b442b3c02bd8ff213
Nguyen Thi Ngoc Mai (K17 HL) <maintnhe176347@fpt.edu.vn>
2023-06-15 01:16:21 +0000

    Revert "Merge branch 'new_namnd(4)' into 'main'"
    This reverts merge request !63

--- src/main/java/swp391/quizpracticing/model/UserSubject.java
+++ src/main/java/swp391/quizpracticing/model/UserSubject.java
@@ -16,23 +16,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSubject {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Integer id;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id",referencedColumnName = "id")
-    private User user;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "subject_id",referencedColumnName = "id")
-    private Subject subject;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "price_package_id", referencedColumnName = "id")
-    private Pricepackage pricePackage;
+    @EmbeddedId
+    private UserSubjectKey id;
 
     @Column(name = "registration_time")
     private Timestamp registrationTime;
@@ -45,10 +30,6 @@ public class UserSubject {
     
     @Column(name="notes")
     private String notes;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="last_updated_by", referencedColumnName = "id")
-    private User userUpdate;
     
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "registration_status_id", referencedColumnName = "id")
