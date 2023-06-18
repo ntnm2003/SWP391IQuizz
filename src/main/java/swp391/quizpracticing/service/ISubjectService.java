f40e9bdd487bea1d82651451af70ed9115a68c7a
Mosena <Mosena@192.168.108.102>
2023-06-19 05:58:45 +0700

    Update sale

--- src/main/java/swp391/quizpracticing/service/ISubjectService.java
+++ src/main/java/swp391/quizpracticing/service/ISubjectService.java
@@ -14,6 +14,7 @@ import java.util.List;
  * @author Mosena
  */
 public interface ISubjectService {
+    
     public Page<SubjectDTO> findPaginatedAllSubjects(int pageNo, int pageSize);
 
     public Page<SubjectDTO> filterSubjectByCategory(int pageNo,
@@ -38,6 +39,8 @@ public interface ISubjectService {
                                                           int pageSize, int category, String subjectName,
                                                           String sortBy, String order);
 
+   List<SubjectDTO> findAll();
+    
     List<Subject> findByFeaturing(Boolean isFeatured);
 
     public List<Subject> listAll();
